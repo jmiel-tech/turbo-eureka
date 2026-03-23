777b37f2843bf4a0427beefef37c83625aeed767
jmiel <jmielk92@gmail.com>
2026-03-23 19:14:11 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "main", "index.html"), (err, content) => {
+    fs.readFile(path.join(__dirname, "index.html"), (err, content) => {
       if (err) throw err;
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(content);
     });
   } else if (req.url === "/api") {
-    fs.readFile(
-      path.join(__dirname, "main", "db.json"),
-      "utf-8",
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(content);
-      },
-    );
+    fs.readFile(path.join(__dirname, "db.json"), "utf-8", (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(content);
+    });
   } else {
     res.end("<h1> page does not exist </h1>");
   }
